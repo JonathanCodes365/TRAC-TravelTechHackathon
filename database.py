5ec4e9966ae213488646e78031ea109c4c6d17f0
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 14:56:29 +0545

    Checking whether DB is connected to postgresql or not

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 
-#create_engine is a SQLAlchemy mechanism that allows for creating a engine.
+#create_engine is a SQLAlchemy mechanism that allows for creating a database engine.
 DATABASE_URL = "postgresql+psycopg://ngawangtharchinsherpa@localhost:5432/trac"
 #it means Use postgresql through psycopg driver.
 engine = create_engine(DATABASE_URL)
+
+with engine.connect() as connection:
+    print("Database Connected")
