50c037d7d35c13ab9fa0c2e12b85d0b87bfefe22
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 15:17:19 +0545

    Tras to schema.py

---
+++ schema.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+#for validation
+from typing import Optional
+#for optional data.
+from enum import Enum
+#this is to make sure that the user can only select from a few range of type we specify.
+
+
+class Reporttype(Enum):
+    RESCUE = "rescue"
+    MISSING = "missing"
+    INJURED = "injured"
+    SAFE = "safe"
+    INCIDENT = "incident"
+
+
+    class Report(BaseModel):
+        type:Reporttype
+        message:str
+        location:Optional[str]=None
+
+#here we create our pydantic checking for the incoming info:
