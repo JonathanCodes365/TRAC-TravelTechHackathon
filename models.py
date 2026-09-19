e3a23f59ab10391a2bcce25101ba94fb293fa7f5
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 15:27:17 +0545

    Changing report to Reportmodel for a better naming convention

--- models.py
+++ models.py
@@ -4,14 +4,14 @@ from sqlalchemy import String,Enum
 #postgresql has many types of column such as VARCHAR, TEXT, Integer, boolean etc.
 #we need to make sure that the column uses String
 
-from schemas import ReportType
+from schemas import Reporttype
 #we want to make sure our type is pydantic.
 
 
 class Base(DeclarativeBase):
     pass
 
-class Report(Base):
+class ReportModel(Base):
     #here we are saying Report = database model and it represent a postgresql table 
     #called reports.
     __tablename__ = "reports"
@@ -21,11 +21,12 @@ class Report(Base):
     #id:Mapped[int] means this model has an id and it is expected to be int.
     #mapped_column means make it a database column
     #primary_key = True means make it a primary key....
+
+    type:Mapped[Reporttype] = mapped_column(Enum(Reporttype))
+        #ok this is us saying type is a part of this model and it must be of the ReportType
+        #next : we prev. did Class ReportType(Enum) now we are saying Enum(ReportType)
+        #which means create a SQL Alchemy Enum based on my ReportType Enum.
+    
     message:Mapped[str] =mapped_column(String)
 
     location:Mapped[str | None] = mapped_column(String,nullable = True)
-
-    type:Mapped[ReportType] = mapped_column(Enum(ReportType))
-    #ok this is us saying type is a part of this model and it must be of the ReportType
-    #next : we prev. did Class ReportType(Enum) now we are saying Enum(ReportType)
-    #which means create a SQL Alchemy Enum based on my ReportType Enum.
