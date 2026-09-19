2debd1bfb305d271bb46d8afbc2ebfd9bb4b1a9a
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 15:27:57 +0545

    Managing indentation

--- schema.py
+++ schema.py
@@ -6,6 +6,10 @@ from enum import Enum
 #this is to make sure that the user can only select from a few range of type we specify.
 
 
+#note schemas is for pydantic schemas.
+
+
+
 class Reporttype(Enum):
     RESCUE = "rescue"
     MISSING = "missing"
@@ -14,9 +18,9 @@ class Reporttype(Enum):
     INCIDENT = "incident"
 
 
-    class Report(BaseModel):
-        type:Reporttype
-        message:str
-        location:Optional[str]=None
+class Report(BaseModel):
+    type:Reporttype
+    message:str
+    location:Optional[str]=None
 
 #here we create our pydantic checking for the incoming info:
