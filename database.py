ca9884ea92edb0f5570004e65931c1996a3c586f
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 15:21:35 +0545

    Adding basemetadata.txt

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
+#after creating the models; we want to make sure to tell PostgreSQL to create corresponding tables
+from models import Base
 
 #create_engine is a SQLAlchemy mechanism that allows for creating a database engine.
 DATABASE_URL = "postgresql+psycopg://ngawangtharchinsherpa@localhost:5432/trac"
 #it means Use postgresql through psycopg driver.
 engine = create_engine(DATABASE_URL)
 
-with engine.connect() as connection:
-    print("Database Connected")
+Base.metadata.create_all(bind=engine)
