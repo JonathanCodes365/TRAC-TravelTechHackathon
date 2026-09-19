c3ac6aa4ab8eae06a35302a6f2bfbdaad1dce848
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-19 15:18:10 +0545

    Making sure that the columns are correct in nature

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 #ensuring we can use Base to point that whoever inherits from Base will be a model.
-from sqlalchemy import String,Integer
+from sqlalchemy import String,Enum
 #postgresql has many types of column such as VARCHAR, TEXT, Integer, boolean etc.
 #we need to make sure that the column uses String
+
+from schemas import ReportType
+#we want to make sure our type is pydantic.
 
 
 class Base(DeclarativeBase):
@@ -19,4 +22,10 @@ class Report(Base):
     #mapped_column means make it a database column
     #primary_key = True means make it a primary key....
     message:Mapped[str] =mapped_column(String)
+
     location:Mapped[str | None] = mapped_column(String,nullable = True)
+
+    type:Mapped[ReportType] = mapped_column(Enum(ReportType))
+    #ok this is us saying type is a part of this model and it must be of the ReportType
+    #next : we prev. did Class ReportType(Enum) now we are saying Enum(ReportType)
+    #which means create a SQL Alchemy Enum based on my ReportType Enum.
